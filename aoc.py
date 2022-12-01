1148cac0d19ee5edabb1323716dc9992b919c631
Denmads <madsj18@student.sdu.dk>
2022-12-01 09:57:21 +0100

    Loggin performance

--- aoc.py
+++ aoc.py
@@ -1,16 +1,27 @@
 import loader
 import sys
+import time
 
 def main(day: str, part: str):
+
+    if part != "a" and part != "b":
+        print("Part can only be 'a' or 'b'")
+        exit(1)
+
     task = loader.load_task(day)
     input_txt = loader.load_input(day)
+    print("")
 
+    start = time.perf_counter()
     if part == "a":
         task.run_a(input_txt)
     elif part == "b":
         task.run_b(input_txt)
-    else:
-        print("Part can only be 'a' or 'b'")
+    
+    exec_time = time.perf_counter() - start
+    print("----------------------")
+    print(f"It took {exec_time} seconds to run!")
+    print("")
 
 
 if __name__ == '__main__':
