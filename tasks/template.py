058e7b8a04445e029557b70ee76047ecc23fea16
Denmads <43139465+Denmads@users.noreply.github.com>
2022-12-07 12:00:22 +0100

    Day 7 Complete

--- tasks/template.py
+++ tasks/template.py
@@ -1,8 +1,12 @@
 from typing import Any
 
+# //////////////////// PARSING /////////////////////////
 
 def parse_input(data: str) -> Any:
     pass
+
+
+# //////////////////// PARTS /////////////////////////
 
 def run_a(data: Any):
     pass
